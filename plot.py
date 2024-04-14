2bc842178077b2e4163273891712767420415cb6
tommydraw <tdraw@umich.edu>
2024-04-14 19:40:32 -0400

    plotting visualizations changes to plot by year instead of by individual title

--- plot.py
+++ plot.py
@@ -14,51 +14,96 @@ def plot_all_news_opinions(filename):
         return
 
     n_articles = len(articles)
-    grid_size = math.ceil(math.sqrt(n_articles))
+    grid_size = int(n_articles / 25)
 
     fig, axs = plt.subplots(grid_size, grid_size, figsize=(16, 9))
     axs = axs.flatten()
 
+    # loop through and aggregate the avg sentiment scores for each year 
+    # - store info in dictionary where keys are the year and the values are a list of scores
+    year_scores_dict = {}
     for i in range(n_articles):
-        ax = axs[i]
         article = articles[i]
-
-        title = article['Title']
-        url = article['URL']
+        article_year = article["Year"]
         sentiment_list = eval(article['Sentiment List'])
-        summary = article['Summary']
 
-        wrapped_title = textwrap.fill(title, width=40)
-        wrapped_summary = textwrap.fill(summary, width=40)
+        if article_year in year_scores_dict:
+            # get sentiment scores
+            sentiment_scores = [float(score) for score in sentiment_list]
+            for score in sentiment_scores:
+                if score < 0:
+                    year_scores_dict[article_year]["Category Counts"][0] += 1
+                elif score == 0:
+                    year_scores_dict[article_year]["Category Counts"][1] += 1
+                else:
+                    year_scores_dict[article_year]["Category Counts"][2] += 1
+        else:
+            year_scores_dict[article_year] = {}
+            # get sentiment scores
+            sentiment_scores = [float(score) for score in sentiment_list]
+            category_counts = [0, 0, 0]
+            for score in sentiment_scores:
+                if score < 0:
+                    category_counts[0] += 1
+                elif score == 0:
+                    category_counts[1] += 1
+                else:
+                    category_counts[2] += 1
+            year_scores_dict[article_year]["Category Counts"] = category_counts
 
-        sentiment_scores = [float(score) for score in sentiment_list]
+    counter = 0
+    for year, year_dict in year_scores_dict.items():
+        ax = axs[counter]
+        counter += 1
+
+        wrapped_title = textwrap.fill(year, width=40)
 
         categories = ['Negative', 'Neutral', 'Positive']
-        category_counts = [0, 0, 0]
-        for score in sentiment_scores:
-            if score < 0:
-                category_counts[0] += 1
-            elif score == 0:
-                category_counts[1] += 1
-            else:
-                category_counts[2] += 1
-
-        ax.bar(categories, category_counts, color=['red', 'grey', 'green'])
+        ax.bar(categories, year_dict["Category Counts"], color=['red', 'grey', 'green'])
         ax.set_title(wrapped_title, fontsize=10, color='blue', picker=True)
-
-        # Separate text objects for label and summary
-        # ax.text(0.5, -0.0, "Public Sentiment Summarized:", transform=ax.transAxes, fontsize=8, ha='center', va='top', weight='bold')
-        ax.text(0.5, -0.9, "Summarized Sentiment: " + wrapped_summary, transform=ax.transAxes, fontsize=8, ha='center', va='top', wrap=True)
-
         ax.set_xlabel('Sentiment Category', fontsize=8)
         ax.set_ylabel('Number of Comments', fontsize=8)
 
-        def on_pick(event):
-            artist = event.artist
-            if isinstance(artist, plt.Text):
-                webbrowser.open(url)
+    # for i in range(n_articles):
+    #     ax = axs[i]
+    #     article = articles[i]
 
-        fig.canvas.mpl_connect('pick_event', on_pick)
+    #     title = article['Title']
+    #     url = article['URL']
+    #     sentiment_list = eval(article['Sentiment List'])
+    #     summary = article['Summary']
+
+    #     wrapped_title = textwrap.fill(title, width=40)
+    #     wrapped_summary = textwrap.fill(summary, width=40)
+
+    #     sentiment_scores = [float(score) for score in sentiment_list]
+
+    #     categories = ['Negative', 'Neutral', 'Positive']
+    #     category_counts = [0, 0, 0]
+    #     for score in sentiment_scores:
+    #         if score < 0:
+    #             category_counts[0] += 1
+    #         elif score == 0:
+    #             category_counts[1] += 1
+    #         else:
+    #             category_counts[2] += 1
+
+    #     ax.bar(categories, category_counts, color=['red', 'grey', 'green'])
+    #     ax.set_title(wrapped_title, fontsize=10, color='blue', picker=True)
+
+    #     # Separate text objects for label and summary
+    #     # ax.text(0.5, -0.0, "Public Sentiment Summarized:", transform=ax.transAxes, fontsize=8, ha='center', va='top', weight='bold')
+    #     ax.text(0.5, -0.9, "Summarized Sentiment: " + wrapped_summary, transform=ax.transAxes, fontsize=8, ha='center', va='top', wrap=True)
+
+    #     ax.set_xlabel('Sentiment Category', fontsize=8)
+    #     ax.set_ylabel('Number of Comments', fontsize=8)
+
+    #     def on_pick(event):
+    #         artist = event.artist
+    #         if isinstance(artist, plt.Text):
+    #             webbrowser.open(url)
+
+    #     fig.canvas.mpl_connect('pick_event', on_pick)
 
     # Hide unused axes
     for ax in axs[n_articles:]:
@@ -68,4 +113,4 @@ def plot_all_news_opinions(filename):
     plt.tight_layout()
     plt.show()
 
-plot_all_news_opinions("output_10.csv")
+plot_all_news_opinions("output.csv")
--- run.py
+++ run.py
@@ -7,25 +7,25 @@ def main():
 
     # data aquisition
     # subprocess.call("python3 setup.py --clear", shell=True) # use flag --clear before running to drop all tables
-    subprocess.call("python3 setup.py", shell=True)
+    #subprocess.call("python3 setup.py", shell=True)
     
     # get 25 new york times article titles and store them in the news database using the New York Times API
-    subprocess.call("python3 get_articles.py", shell=True)
+    #subprocess.call("python3 get_articles.py", shell=True)
 
     # search for 5 fetched new york times article titles on reddit and gather 5 comments from each article's 5 most relevant posts using PRAW API.
     # Stores 25 articles each time, so we need to run this 5 times for 25 articles.
-    for i in range(0,5):
-        subprocess.call("python3 search_reddit.py", shell=True)
+    # for i in range(0,5):
+    #     subprocess.call("python3 search_reddit.py", shell=True)
 
     # fetch the reddit post titles and comments associated with each new york times article and summarize the article with the opinions in context with Azure API
-    subprocess.call("python3 get_summaries.py", shell=True)
+    #subprocess.call("python3 get_summaries.py", shell=True)
 
     # join article titles, post titles, post comments, and summaries together and dump to csv file for visualization
-    subprocess.call("python3 dump_to_csv.py", shell=True)
+    #subprocess.call("python3 dump_to_csv.py", shell=True)
 
 
     # generate figures based on the data we've outputted to our csv file
-    # subprocess.call("python3 ploy.py", shell=True)
+     subprocess.call("python3 plot.py", shell=True)
 
 
 if __name__ == "__main__":
